6c20a4303ef12a0b8133b11de36301aaa25fefc2
Abdullah As Sakib <sakib5800.sk.3@gmail.com>
2024-08-29 22:11:02 +0600

    Added

--- ForEachloop/src/ForEachloop.java
+++ ForEachloop/src/ForEachloop.java
@@ -3,11 +3,11 @@ public class ForEachloop {
         /*There is also a "for-each" loop, which is used exclusively to loop through elements in an array:*/
 
         /*for (type variableName : arrayName) {
-            // code block to be executed
+            code block to be executed
         }*/
         String [] students= {"Abdullah", "Enam", "Sadik", "Sakib", "Sazid", "Jafor", "Sohan"};
         for (String i : students) {
-            System.out.println(i);
+            System.out.print(i+  " ");
         }
     }
 }
--- Temp/src/Temp.java
+++ Temp/src/Temp.java
@@ -1,43 +1,9 @@
+
+package com.mycompany.w3resource;
 public class Temp {
     public static void main(String[] args) {
-        int target = 0;
-        int[][] matrix = new int[][] {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
-        };
 
-        for (int[] row : matrix) {
-            for (int col : row) {
-                System.out.print(col + " ");
-            }
-            System.out.println();
-        }
-
-        System.out.print(searchMatrix(matrix, target));
+        System.out.println("Hello\nSakib");
     }
 
-    public static boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix.length == 0 || matrix[0].length == 0) {
-            return false;
-        }
-
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int low = 0, high = m * n - 1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            int midVal = matrix[mid / n][mid % n];
-            if (midVal == target) {
-                return true;
-            } else if (midVal < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-
-        return false;
-    }
 }
